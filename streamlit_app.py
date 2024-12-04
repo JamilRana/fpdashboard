7b0dabda1cda48fd5c4f31ff35f2831f11e93aa3
JamilRana <83330448+JamilRana@users.noreply.github.com>
2024-12-04 10:57:32 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,14 @@ st.title('FP Dashboard')
 
 st.info('MR Pack and Family planning dashboard')
 
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+with st.expender('Data'):
+ st.write('**Raw Data**')
+ df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+ df 
+
+st.write('**X**')
+x = df.drop('species', axis =1)
+x
+
+
  
