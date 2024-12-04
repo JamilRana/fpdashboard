ef0edb96ede352f80e80513f49af71233561992a
JamilRana <83330448+JamilRana@users.noreply.github.com>
2024-12-04 10:53:28 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('FP Dashboard')
 
-st.write('Hello world!')
+st.info('MR Pack and Family planning dashboard')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+ 
