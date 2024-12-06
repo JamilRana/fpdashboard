5de4ecdc1bd60d133d58203a2963c3801128fb18
JamilRana <83330448+JamilRana@users.noreply.github.com>
2024-12-06 12:18:19 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  x_raw = df.drop('species',axis=1)
-  x_raw
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
   st.write('**y**')
   y_raw = df.species
